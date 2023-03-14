879aca2fce285348135a4fa405f5aa610ad30412
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-03-14 14:06:07 -0300

    add: some extra global styles

--- src/style/GlobalStyle.js
+++ src/style/GlobalStyle.js
@@ -1,19 +1,26 @@
 import { createGlobalStyle } from "styled-components"
 
 const GlobalStyle = createGlobalStyle`
-    #root {
-        * {
-            box-sizing: border-box;
-            font-family: 'Lexend Deca', sans-serif;
-            font-weight: 400;
-        }
-        button {
-            cursor:pointer;
-        }
-        a {
-            text-decoration:none;
-        }
+    html, body, #root {
+        width: 100%;
+        height:100%;
     }
+    * {
+        box-sizing: border-box;
+        font-family: 'Lexend Deca', sans-serif;
+        font-weight: 400;
+        color: #666666;
+    }
+    & div {
+        width:100%;
+        background-color:#f2f2f2;
+    }
+    button {
+        cursor:pointer;
+    }
+    a {
+        text-decoration:none;
+    }    
 `
 
 export default GlobalStyle
