1a51784072f02968d416127caaaba91ab68ae148
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-03-20 08:19:30 -0300

    add Shared to Route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import { Shared } from "./components/Shared";
 import { HabitsPage } from "./pages/HabitsPage";
 import { HistoryPage } from "./pages/HistoryPage";
 import { HomePage } from "./pages/HomePage";
@@ -9,10 +10,10 @@ function App() {
   return (
     <Routes>
       <Route path='/' element={<HomePage />} />
-      <Route path='/habitos' element={<HabitsPage />} />
+      <Route path='/habitos' element={<Shared><HabitsPage /></Shared>} />
       <Route path='/cadastro' element={<SignUpPage />} />
-      <Route path='/hoje' element={<TodaysHabitsPage />} />
-      <Route path='/historico' element={<HistoryPage />} />
+      <Route path='/hoje' element={<Shared><TodaysHabitsPage /></Shared>} />
+      <Route path='/historico' element={<Shared><HistoryPage /></Shared>} />
       <Route path='*' element="404"/> 
     </Routes>
   );
